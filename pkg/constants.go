5791321b03e9e09864f10daed69b045bd4e94cef
John Dietz <john@kubefirst.com>
2022-10-23 12:06:54 -0400

    updating datadog metric and extending readiness probe period

--- pkg/constants.go
+++ pkg/constants.go
@@ -1,4 +1,4 @@
 package pkg
 
 const SegmentIOWriteKey = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
-const MetricHealth = "kubefirst.gh.mgmt.kubefirst.com.healthz"
+const MetricHealth = "kubefirst.kubefirst_api.healthz"
