cca5f558a7f1061dcd1950dc2873d9d7979969a5
Robert Keith Rippetoe <robertkeithrippetoe@Roberts-MBP.domain>
2023-01-30 14:49:44 -0700

    feat(metric): reorder new property placment

--- internal/domain/telemetry.go
+++ internal/domain/telemetry.go
@@ -25,18 +25,19 @@ func NewTelemetry(metricName string, domain string, CLIVersion string) (Telemetr
 		return Telemetry{}, errors.New("unable to create metric, missing metric name")
 	}
 
+	// scan for kubefirst_team env
+	kubeFirstTeam := "false"
+	if os.Getenv("kubefirst_team") == "true" {
+		kubeFirstTeam = "true"
+	}
+
 	// localhost installation doesn't provide hostedzone that are mainly used as domain in this context. In case a
 	// hostedzone is not provided, we assume it's a localhost installation
-	kubeFirstTeam := "false"
 	if len(domain) == 0 {
-		uniqueUserId := uuid.New()
-		domain = uniqueUserId.String()
-		if os.Getenv("kubefirst_team") == "true" {
-			kubeFirstTeam = "true"
-		}
+		clusterId := uuid.New().String()
 		return Telemetry{
 			MetricName:    metricName,
-			Domain:        domain,
+			Domain:        clusterId,
 			CLIVersion:    CLIVersion,
 			KubeFirstTeam: kubeFirstTeam,
 			ClusterType:   "mgmt",
